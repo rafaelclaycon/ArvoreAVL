a7f12a4513c97954c9e60c5aa6207a08cacd8474
Rafael Claycon Schmitt <rafael.claycon@gmail.com>
2020-09-17 19:51:20 -0300

    Primeiro commit

---
+++ ArvoreAVL/ArvoreAVLApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArvoreAVLApp.swift
+//  ArvoreAVL
+//
+//  Created by Rafael Schmitt on 17/09/20.
+//
+
+import SwiftUI
+
+@main
+struct ArvoreAVLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
