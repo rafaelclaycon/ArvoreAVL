99a03b94fe112a533b478cea80cc7e0504bfb8bb
Rafael Claycon Schmitt <rafael.claycon@gmail.com>
2020-09-18 10:30:41 -0300

    Adiciona view para árvore

--- ArvoreAVL/DiagramaArvore.swift
+++ ArvoreAVL/DiagramaArvore.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct Arvore: View {
+struct DiagramaArvore: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
@@ -15,6 +15,6 @@ struct Arvore: View {
 
 struct Arvore_Previews: PreviewProvider {
     static var previews: some View {
-        Arvore()
+        DiagramaArvore()
     }
 }
