8a8c566f903700d25937d2306bbd866eb7b99fca
Rafael Claycon Schmitt <rafael.claycon@gmail.com>
2020-09-17 22:37:49 -0300

    Adiciona estrutura básica da tela principal

--- ArvoreAVL/ContentView.swift
+++ ArvoreAVL/ContentView.swift
@@ -8,9 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var entrada = ""
+    
     var body: some View {
-        Text("Hello, world!")
+        VStack(alignment: .leading) {
+            Text("Árvore AVL de Inteiros")
+                .font(.title)
+                .bold()
+                .padding()
+                .padding(.leading, 10)
+            
+            Spacer()
+            
+            HStack {
+                Spacer()
+                
+                TextField("Número inteiro", text: $entrada)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .keyboardType(.numberPad)
+                    .frame(width: 150)
+                    .padding(.trailing, 20)
+                
+                Button(action: {
+                    print("Número adicionado à árvore.")
+                }) {
+                    HStack {
+                        Image(systemName: "plus")
+                        Text("Adicionar")
+                    }
+                    .padding(.vertical, 8)
+                    .padding(.horizontal, 15)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10.0)
+                            .stroke(lineWidth: 1.0)
+                    )
+                }
+                
+                Spacer()
+            }
             .padding()
+        }
     }
 }
 
