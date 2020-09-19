daa1947d597215e1942132c1c2f8cd169cf5bd3b
Rafael Claycon Schmitt <rafael.claycon@gmail.com>
2020-09-19 12:12:53 -0300

    Mudanças de tela e começo lógica inserção

---
+++ ArvoreAVL/ImageStore.swift
@@ -0,0 +1,8 @@
+//
+//  ImageStore.swift
+//  ArvoreAVL
+//
+//  Created by Rafael Schmitt on 18/09/20.
+//
+
+import Foundation
---
+++ ArvoreAVL/String+isInt.swift
@@ -0,0 +1,8 @@
+//
+//  String+isInt.swift
+//  ArvoreAVL
+//
+//  Created by Rafael Schmitt on 19/09/20.
+//
+
+import Foundation
